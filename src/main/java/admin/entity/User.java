cb4aed26e2aec0409232f5164c68624d8a22fe3e
Moloth <moloth_@mail.ru>
2019-08-18 08:20:08 +0300

    Add google code style #1

--- src/main/java/admin/entity/User.java
+++ src/main/java/admin/entity/User.java
@@ -2,12 +2,16 @@ package admin.entity;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import java.time.LocalDate;
 
 /**
  * data class for table "user": see README.md for its description
